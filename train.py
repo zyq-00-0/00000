686944fff52d88ce25f02e786ff9e7ba6019fa17
zyq-00-0 <3159745883@qq.com>
2025-06-22 19:30:56 +0800

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     model.train(data='/root/dataset/dataset_visdrone/data.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=200,
+                epochs=100,
                 batch=32,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
